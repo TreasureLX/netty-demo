f7b96a8c568a1a50abbc9648012a9c4d5cd1af91
LockedX <1130876885@qq.com>
2019-06-25 17:01:00 +0800

    新增thrift

---
+++ netty-study/src/main/java/com/lanxing/netty/demo7/client/ThriftClient.java
@@ -0,0 +1,29 @@
+package com.lanxing.netty.demo7.client;
+
+import com.lanxing.netty.demo7.Person;
+import com.lanxing.netty.demo7.PersonService;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+public class ThriftClient {
+    public static void main(String[] args) {
+        TTransport tTransport=new TFramedTransport(new TSocket("localhost",8899),600);
+        TProtocol protocol=new TCompactProtocol(tTransport);
+        PersonService.Client client=new PersonService.Client(protocol);
+
+        try {
+            tTransport.open();
+            Person person=client.getPersonByUsername("111");
+            System.out.println(person);
+            client.savePerson(new Person().setAge(24).setMarried(true).setName("test"));
+        } catch (TException e) {
+            e.printStackTrace();
+        }finally {
+            tTransport.close();
+        }
+    }
+}
---
+++ netty-study/src/main/java/com/lanxing/netty/demo7/server/impl/PersonServiceImpl.java
@@ -0,0 +1,18 @@
+package com.lanxing.netty.demo7.server.impl;
+
+import com.lanxing.netty.demo7.DataException;
+import com.lanxing.netty.demo7.Person;
+import com.lanxing.netty.demo7.PersonService;
+import org.apache.thrift.TException;
+
+public class PersonServiceImpl implements PersonService.Iface {
+    @Override
+    public Person getPersonByUsername(String username) throws DataException, TException {
+        return new Person().setAge(20).setMarried(false).setName(username);
+    }
+
+    @Override
+    public void savePerson(Person person) throws DataException, TException {
+        System.out.println("save person:"+person.toString());
+    }
+}
